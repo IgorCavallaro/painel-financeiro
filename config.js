f73423ad96adb2ef9014a57c0b7d82f2776816e6
Igor Cavallaro <30940447+IgorCavallaro@users.noreply.github.com>
2026-08-13 08:37:12 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Copiado de config.example.js e preenchido com a URL real do Apps Script.
+window.APP_CONFIG = {
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbyiAiBuJC28D84EdhXpr_F6ulcGxIKgDZk8vBThh05tAxrLEcnFiz5IKIQGgmmTxQYf/exec'
+};
